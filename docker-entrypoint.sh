e152c7c59366041e3acd79fae72ce04ce609da10
soxueren <250029975@qq.com>
2020-03-13 16:40:00 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 cd /openstreetmap-website && bundle exec rake db:create
 
-psql -d osm -h $PG_HOST --username $POSTGRES_USER -f /openstreetmap-website/db/extensions.sql
+psql -d osm -h $PGHOST -p $PGPORT --username $PGUSER -f /openstreetmap-website/db/extensions.sql
 
-psql -d osm -h $PG_HOST --username $POSTGRES_USER -f /openstreetmap-website/db/functions/functions.sql
+psql -d osm -h $PGHOST -p $PGPORT --username $PGUSER -f /openstreetmap-website/db/functions/functions.sql
 
 cd /openstreetmap-website  && bundle exec rake db:migrate && bundle exec rake test:db
 
