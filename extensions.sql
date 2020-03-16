bbad25ac6354b00d8a367069f9e742fd49722ab6
soxueren <250029975@qq.com>
2020-03-16 11:04:46 +0800

    Update extensions.sql

--- extensions.sql
+++ extensions.sql
@@ -1,24 +1,21 @@
 -- Extension: btree_gist
 
--- DROP EXTENSION btree_gist;
-
+DROP EXTENSION btree_gist;
 CREATE EXTENSION btree_gist
     SCHEMA public
     VERSION "1.2";
 
 -- Extension: hstore
 
--- DROP EXTENSION hstore;
-
+DROP EXTENSION hstore;
 CREATE EXTENSION hstore
     SCHEMA public
     VERSION "1.4";
 
 -- Extension: postgis
 
--- DROP EXTENSION postgis;
-
+DROP EXTENSION postgis;
 CREATE EXTENSION postgis
     SCHEMA public
     VERSION "2.5.1";
-© 2020 GitHub, Inc.
+
