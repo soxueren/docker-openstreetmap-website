506ad5c3eac99f4edfbaac96b7e10fc59bbfc15c
soxueren <250029975@qq.com>
2020-03-16 11:30:21 +0800

    change extensions.sql
    extensions.sql

--- extensions.sql
+++ extensions.sql
@@ -1,15 +1,15 @@
 -- Extension: btree_gist
-DROP EXTENSION btree_gist CASCADE;
+-- DROP EXTENSION btree_gist CASCADE;
 CREATE EXTENSION btree_gist
     SCHEMA public;
 
 -- Extension: hstore
-DROP EXTENSION hstore CASCADE;
+-- DROP EXTENSION hstore CASCADE;
 CREATE EXTENSION hstore
     SCHEMA public;
 
 
 -- Extension: postgis
-DROP EXTENSION postgis CASCADE;
+-- DROP EXTENSION postgis CASCADE;
 CREATE EXTENSION postgis 
     SCHEMA public;
